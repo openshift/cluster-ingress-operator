5b54d867c423ef4e13537b3359240ab5795b5074
Dan Mace <ironcladlou@gmail.com>
2018-08-07 10:09:58 -0400

    Launch a router behind a Service LB

--- pkg/stub/handler.go
+++ pkg/stub/handler.go
@@ -6,12 +6,15 @@ import (
 	"github.com/openshift/cluster-ingress-operator/pkg/apis/ingress/v1alpha1"
 
 	"github.com/operator-framework/operator-sdk/pkg/sdk"
+
 	"github.com/sirupsen/logrus"
+
 	appsv1 "k8s.io/api/apps/v1"
 	corev1 "k8s.io/api/core/v1"
 	"k8s.io/apimachinery/pkg/api/errors"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/runtime/schema"
+	"k8s.io/apimachinery/pkg/util/intstr"
 )
 
 func NewHandler() sdk.Handler {
@@ -23,11 +26,19 @@ type Handler struct {
 }
 
 func (h *Handler) Handle(ctx context.Context, event sdk.Event) error {
+	if event.Deleted {
+		return nil
+	}
 	switch o := event.Object.(type) {
 	case *v1alpha1.ClusterIngress:
 		err := sdk.Create(newRouterDaemonSet(o))
 		if err != nil && !errors.IsAlreadyExists(err) {
-			logrus.Errorf("Failed to create busybox pod : %v", err)
+			logrus.Errorf("Failed to create daemonset: %v", err)
+			return err
+		}
+		err = sdk.Create(newRouterService(o))
+		if err != nil && !errors.IsAlreadyExists(err) {
+			logrus.Errorf("Failed to create service: %v", err)
 			return err
 		}
 	}
@@ -68,15 +79,100 @@ func newRouterDaemonSet(cr *v1alpha1.ClusterIngress) *appsv1.DaemonSet {
 					},
 				},
 				Spec: corev1.PodSpec{
+					ServiceAccountName: "router",
 					NodeSelector: map[string]string{
 						"node-role.kubernetes.io/infra": "true",
 					},
 					Containers: []corev1.Container{
 						{
-							Name:    "busybox",
-							Image:   "busybox",
-							Command: []string{"sleep", "3600"},
+							Name:  "router",
+							Image: "docker.io/openshift/origin-haproxy-router:v3.11.0",
+							Ports: []corev1.ContainerPort{
+								{
+									Name:          "http",
+									ContainerPort: 80,
+									Protocol:      "TCP",
+								},
+								{
+									Name:          "https",
+									ContainerPort: 443,
+									Protocol:      "TCP",
+								},
+								{
+									Name:          "stats",
+									ContainerPort: 1936,
+									Protocol:      "TCP",
+								},
+							},
+							Env: []corev1.EnvVar{
+								{Name: "STATS_PORT", Value: "1936"},
+							},
+							LivenessProbe: &corev1.Probe{
+								InitialDelaySeconds: 10,
+								Handler: corev1.Handler{
+									HTTPGet: &corev1.HTTPGetAction{
+										Path: "/healthz",
+										Port: intstr.IntOrString{
+											Type:   intstr.Int,
+											IntVal: int32(1936),
+										},
+									},
+								},
+							},
+							ReadinessProbe: &corev1.Probe{
+								InitialDelaySeconds: 10,
+								Handler: corev1.Handler{
+									HTTPGet: &corev1.HTTPGetAction{
+										Path: "/healthz",
+										Port: intstr.IntOrString{
+											Type:   intstr.Int,
+											IntVal: int32(1936),
+										},
+									},
+								},
+							},
 						},
+					},
+				},
+			},
+		},
+	}
+}
+
+func newRouterService(cr *v1alpha1.ClusterIngress) *corev1.Service {
+	name := "router-" + cr.Name
+	return &corev1.Service{
+		TypeMeta: metav1.TypeMeta{
+			Kind:       "Service",
+			APIVersion: "v1",
+		},
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      name,
+			Namespace: cr.Namespace,
+			OwnerReferences: []metav1.OwnerReference{
+				*metav1.NewControllerRef(cr, schema.GroupVersionKind{
+					Group:   v1alpha1.SchemeGroupVersion.Group,
+					Version: v1alpha1.SchemeGroupVersion.Version,
+					Kind:    "ClusterIngress",
+				}),
+			},
+			Labels: map[string]string{
+				"app": "router",
+			},
+		},
+		Spec: corev1.ServiceSpec{
+			Type: corev1.ServiceTypeLoadBalancer,
+			Selector: map[string]string{
+				"app": name,
+			},
+			Ports: []corev1.ServicePort{
+				{
+					Name:     "http",
+					Protocol: corev1.ProtocolTCP,
+					Port:     80,
+					TargetPort: intstr.IntOrString{
+						Type:   intstr.String,
+						StrVal: "http",
 					},
 				},
 			},
