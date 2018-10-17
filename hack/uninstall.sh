124ec77bd399d91344b1ca6ef097ee24731e4684
Dan Mace <ironcladlou@gmail.com>
2018-10-17 07:30:21 -0400

    Clean up uninstall script

--- hack/uninstall.sh
+++ hack/uninstall.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
-set -euo pipefail
+set -uo pipefail
 
 # Disable the CVO
-oc patch -n openshift-cluster-version daemonsets/cluster-version-operator --patch '{"spec": {"template": {"spec": {"nodeSelector": {"node-role.kubernetes.io/fake": ""}}}}}' || true
+oc patch -n openshift-cluster-version daemonsets/cluster-version-operator --patch '{"spec": {"template": {"spec": {"nodeSelector": {"node-role.kubernetes.io/fake": ""}}}}}'
 
 # Uninstall tectonic ingress
-oc delete namespaces/openshift-ingress || true
+oc delete namespaces/openshift-ingress
 
-# Uninstall cluster-dns-operator
-oc delete --force --grace-period 0 -n openshift-cluster-ingress-operator clusteringresses/default || true
-oc delete namespaces/openshift-cluster-ingress-operator || true
-oc delete namespaces/openshift-cluster-ingress-router || true
-oc delete clusterroles/cluster-ingress-operator:operator || true
-oc delete clusterroles/cluster-ingress:router || true
-oc delete clusterrolebindings/cluster-ingress-operator:operator || true
-oc delete clusterrolebindings/cluster-ingress:router || true
-oc delete customresourcedefinition.apiextensions.k8s.io/clusteringresses.ingress.openshift.io || true
+# Uninstall the cluster-ingress-operator
+oc delete --force --grace-period 0 -n openshift-cluster-ingress-operator clusteringresses/default
+oc delete namespaces/openshift-cluster-ingress-operator
+oc delete namespaces/openshift-cluster-ingress-router
+oc delete clusterroles/cluster-ingress-operator:operator
+oc delete clusterroles/cluster-ingress:router
+oc delete clusterrolebindings/cluster-ingress-operator:operator
+oc delete clusterrolebindings/cluster-ingress:router
+oc delete customresourcedefinition.apiextensions.k8s.io/clusteringresses.ingress.openshift.io
