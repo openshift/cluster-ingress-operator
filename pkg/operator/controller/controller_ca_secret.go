f369bc8aa307e2ba7a3f1048e558aac64a3008c3
Miciah Masters <miciah.masters@gmail.com>
2019-02-14 19:02:45 -0500

    Refactor router CA secret handling
    Refactor ensureRouterCACertificateSecret in terms of reconciling current
    and desired state.
    
    * pkg/operator/controller/controller_ca_secret.go (routerCASecretName): New
    function that returns the namespaced name of the secret.
    (ensureRouterCACertificateSecret): Get current secret using the new
    currentRouterCASecret method.  If it doesn't exist, create it using the new
    desiredRouterCASecret function and createRouterCASecret method.
    (currentRouterCASecret): New method.
    (desiredRouterCASecret): New function.
    (createRouterCASecret): New method.

--- pkg/operator/controller/controller_ca_secret.go
+++ pkg/operator/controller/controller_ca_secret.go
@@ -22,45 +22,84 @@ const (
 	caCertSecretName = "router-ca"
 )
 
+// routerCASecretName returns the namespaced name for the router CA secret.
+func routerCASecretName(namespace string) types.NamespacedName {
+	return types.NamespacedName{
+		Namespace: namespace,
+		Name:      caCertSecretName,
+	}
+}
+
 // ensureRouterCACertificateSecret ensures a CA certificate secret exists that
 // can be used to sign the default certificates for ClusterIngresses.
 func (r *reconciler) ensureRouterCACertificateSecret() (*corev1.Secret, error) {
+	current, err := r.currentRouterCASecret()
+	if err != nil {
+		return nil, err
+	}
+	if current != nil {
+		return current, nil
+	}
+	desired, err := desiredRouterCASecret(r.Namespace)
+	if err != nil {
+		return nil, err
+	}
+	if err := r.createRouterCASecret(desired); err != nil {
+		return nil, fmt.Errorf("failed to create router CA secret: %v", err)
+	}
+	return r.currentRouterCASecret()
+}
+
+// currentRouterCASecret returns the current router CA secret.
+func (r *reconciler) currentRouterCASecret() (*corev1.Secret, error) {
+	name := routerCASecretName(r.Namespace)
+	secret := &corev1.Secret{}
+	if err := r.Client.Get(context.TODO(), name, secret); err != nil {
+		if errors.IsNotFound(err) {
+			return nil, nil
+		}
+		return nil, err
+	}
+	return secret, nil
+}
+
+// desiredRouterCASecret returns the desired router CA secret.  Note: Because
+// the secret has a generated certificate, this function is non-deterministic.
+func desiredRouterCASecret(namespace string) (*corev1.Secret, error) {
+	// TODO Use certrotationcontroller from library-go.
+	signerName := fmt.Sprintf("%s@%d", "cluster-ingress-operator", time.Now().Unix())
+	caConfig, err := crypto.MakeCAConfig(signerName, 0)
+	if err != nil {
+		return nil, fmt.Errorf("failed to make CA config: %v", err)
+	}
+
+	certBytes, keyBytes, err := caConfig.GetPEMBytes()
+	if err != nil {
+		return nil, fmt.Errorf("failed to get certificate: %v", err)
+	}
+
+	name := routerCASecretName(namespace)
 	secret := &corev1.Secret{
 		ObjectMeta: metav1.ObjectMeta{
-			Name:      caCertSecretName,
-			Namespace: r.Namespace,
+			Name:      name.Name,
+			Namespace: name.Namespace,
 		},
-	}
-	err := r.Client.Get(context.TODO(), types.NamespacedName{Namespace: secret.Namespace, Name: secret.Name}, secret)
-	if err != nil {
-		if !errors.IsNotFound(err) {
-			return nil, fmt.Errorf("failed to get secret %s/%s: %v", secret.Namespace, secret.Name, err)
-		}
-
-		// TODO Use certrotationcontroller from library-go.
-		signerName := fmt.Sprintf("%s@%d", "cluster-ingress-operator", time.Now().Unix())
-		caConfig, err := crypto.MakeCAConfig(signerName, 0)
-		if err != nil {
-			return nil, fmt.Errorf("failed to make CA config: %v", err)
-		}
-
-		secret.Type = corev1.SecretTypeTLS
-		certBytes, keyBytes, err := caConfig.GetPEMBytes()
-		if err != nil {
-			return nil, fmt.Errorf("failed to get certificate: %v", err)
-		}
-		secret.Data = map[string][]byte{
+		Data: map[string][]byte{
 			"tls.crt": certBytes,
 			"tls.key": keyBytes,
-		}
-		if err := r.Client.Create(context.TODO(), secret); err != nil {
-			return nil, fmt.Errorf("failed to create secret %s/%s: %v", secret.Namespace, secret.Name, err)
-		}
-
-		logrus.Infof("created secret %s/%s", secret.Namespace, secret.Name)
+		},
+		Type: corev1.SecretTypeTLS,
 	}
-
 	return secret, nil
+}
+
+// createRouterCASecret creates the router CA secret.
+func (r *reconciler) createRouterCASecret(secret *corev1.Secret) error {
+	if err := r.Client.Create(context.TODO(), secret); err != nil {
+		return err
+	}
+	logrus.Infof("created secret %s/%s", secret.Namespace, secret.Name)
+	return nil
 }
 
 // getRouterCA gets the CA, or creates it if it does not already exist.
