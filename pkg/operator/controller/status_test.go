652c63d7ddf853ccd20f692bd85e1473b3ba1755
Miciah Masters <miciah.masters@gmail.com>
2019-02-28 20:48:11 -0500

    Simplify ClusterOperatorStatus comparison
    Replace util.ElementsEqual by using go-cmp's cmpopts.EquateEmpty and
    cmpopts.SortSlices options.
    
    * pkg/operator/controller/status.go (syncOperatorStatus): Move comparison
    of old and new ClusterOperatorStatus values from here...
    (statusesEqual): ...to this new function.  Replace util.ElementsEqual by
    using cmp.Equal with suitable cmpopts.IgnoreFields, cmpopts.EquateEmpty,
    and cmpopts.SortSlices options.
    * pkg/util/util.go (ElementsEqual): Delete file and function.
    * pkg/util/util_test.go (TestElementsEqual): Move test...
    * pkg/operator/controller/status_test.go (TestStatusesEqual): ...to here.
    Test statusesEqual instead of util.ElementsEqual.

--- pkg/operator/controller/status_test.go
+++ pkg/operator/controller/status_test.go
@@ -4,12 +4,8 @@ import (
 	"fmt"
 	"testing"
 
-	"github.com/google/go-cmp/cmp"
-	"github.com/google/go-cmp/cmp/cmpopts"
-
 	configv1 "github.com/openshift/api/config/v1"
 	ingressv1alpha1 "github.com/openshift/cluster-ingress-operator/pkg/apis/ingress/v1alpha1"
-	"github.com/openshift/cluster-ingress-operator/pkg/util"
 
 	appsv1 "k8s.io/api/apps/v1"
 	corev1 "k8s.io/api/core/v1"
@@ -212,16 +208,243 @@ func TestSetStatusCondition(t *testing.T) {
 
 	for _, tc := range testCases {
 		actual := setStatusCondition(tc.oldConditions, tc.newCondition)
-		opts := []cmp.Option{
-			cmpopts.IgnoreFields(configv1.ClusterOperatorStatusCondition{}, "LastTransitionTime"),
-		}
-		equal, err := util.ElementsEqual(actual, tc.expected, opts)
-		if err != nil {
-			t.Fatalf("%q failed: %v", tc.description, err)
-		}
-		if !equal {
+		a := configv1.ClusterOperatorStatus{Conditions: actual}
+		b := configv1.ClusterOperatorStatus{Conditions: tc.expected}
+		if !statusesEqual(a, b) {
 			t.Fatalf("%q: expected %v, got %v", tc.description,
 				tc.expected, actual)
+		}
+	}
+}
+
+func TestStatusesEqual(t *testing.T) {
+	testCases := []struct {
+		description string
+		expected    bool
+		a, b        configv1.ClusterOperatorStatus
+	}{
+		{
+			description: "zero-valued ClusterOperatorStatus should be equal",
+			expected:    true,
+		},
+		{
+			description: "nil and non-nil slices are equal",
+			expected:    true,
+			a: configv1.ClusterOperatorStatus{
+				Conditions: []configv1.ClusterOperatorStatusCondition{},
+			},
+		},
+		{
+			description: "empty slices should be equal",
+			expected:    true,
+			a: configv1.ClusterOperatorStatus{
+				Conditions: []configv1.ClusterOperatorStatusCondition{},
+			},
+			b: configv1.ClusterOperatorStatus{
+				Conditions: []configv1.ClusterOperatorStatusCondition{},
+			},
+		},
+		{
+			description: "check no change in versions",
+			expected:    true,
+			a: configv1.ClusterOperatorStatus{
+				Versions: []configv1.OperandVersion{
+					{
+						Name:    "operator",
+						Version: "v1",
+					},
+					{
+						Name:    "router",
+						Version: "v2",
+					},
+				},
+			},
+			b: configv1.ClusterOperatorStatus{
+				Versions: []configv1.OperandVersion{
+					{
+						Name:    "operator",
+						Version: "v1",
+					},
+					{
+						Name:    "router",
+						Version: "v2",
+					},
+				},
+			},
+		},
+		{
+			description: "condition LastTransitionTime should be ignored",
+			expected:    true,
+			a: configv1.ClusterOperatorStatus{
+				Conditions: []configv1.ClusterOperatorStatusCondition{
+					{
+						Type:               configv1.OperatorAvailable,
+						Status:             configv1.ConditionTrue,
+						LastTransitionTime: metav1.Unix(0, 0),
+					},
+				},
+			},
+			b: configv1.ClusterOperatorStatus{
+				Conditions: []configv1.ClusterOperatorStatusCondition{
+					{
+						Type:               configv1.OperatorAvailable,
+						Status:             configv1.ConditionTrue,
+						LastTransitionTime: metav1.Unix(1, 0),
+					},
+				},
+			},
+		},
+		{
+			description: "order of versions should not matter",
+			expected:    true,
+			a: configv1.ClusterOperatorStatus{
+				Versions: []configv1.OperandVersion{
+					{
+						Name:    "operator",
+						Version: "v1",
+					},
+					{
+						Name:    "router",
+						Version: "v2",
+					},
+				},
+			},
+			b: configv1.ClusterOperatorStatus{
+				Versions: []configv1.OperandVersion{
+					{
+						Name:    "router",
+						Version: "v2",
+					},
+					{
+						Name:    "operator",
+						Version: "v1",
+					},
+				},
+			},
+		},
+		{
+			description: "check missing related objects",
+			expected:    false,
+			a: configv1.ClusterOperatorStatus{
+				RelatedObjects: []configv1.ObjectReference{
+					{
+						Name: "openshift-ingress",
+					},
+					{
+						Name: "default",
+					},
+				},
+			},
+			b: configv1.ClusterOperatorStatus{
+				RelatedObjects: []configv1.ObjectReference{
+					{
+						Name: "default",
+					},
+				},
+			},
+		},
+		{
+			description: "check extra related objects",
+			expected:    false,
+			a: configv1.ClusterOperatorStatus{
+				RelatedObjects: []configv1.ObjectReference{
+					{
+						Name: "default",
+					},
+				},
+			},
+			b: configv1.ClusterOperatorStatus{
+				RelatedObjects: []configv1.ObjectReference{
+					{
+						Name: "openshift-ingress",
+					},
+					{
+						Name: "default",
+					},
+				},
+			},
+		},
+		{
+			description: "check condition reason differs",
+			expected:    false,
+			a: configv1.ClusterOperatorStatus{
+				Conditions: []configv1.ClusterOperatorStatusCondition{
+					{
+						Type:   configv1.OperatorAvailable,
+						Status: configv1.ConditionFalse,
+						Reason: "foo",
+					},
+				},
+			},
+			b: configv1.ClusterOperatorStatus{
+				Conditions: []configv1.ClusterOperatorStatusCondition{
+					{
+						Type:   configv1.OperatorAvailable,
+						Status: configv1.ConditionFalse,
+						Reason: "bar",
+					},
+				},
+			},
+		},
+		{
+			description: "check duplicate with single condition",
+			expected:    false,
+			a: configv1.ClusterOperatorStatus{
+				Conditions: []configv1.ClusterOperatorStatusCondition{
+					{
+						Type:    configv1.OperatorAvailable,
+						Message: "foo",
+					},
+				},
+			},
+			b: configv1.ClusterOperatorStatus{
+				Conditions: []configv1.ClusterOperatorStatusCondition{
+					{
+						Type:    configv1.OperatorAvailable,
+						Message: "foo",
+					},
+					{
+						Type:    configv1.OperatorAvailable,
+						Message: "foo",
+					},
+				},
+			},
+		},
+		{
+			description: "check duplicate with multiple conditions",
+			expected:    false,
+			a: configv1.ClusterOperatorStatus{
+				Conditions: []configv1.ClusterOperatorStatusCondition{
+					{
+						Type: configv1.OperatorAvailable,
+					},
+					{
+						Type: configv1.OperatorProgressing,
+					},
+					{
+						Type: configv1.OperatorAvailable,
+					},
+				},
+			},
+			b: configv1.ClusterOperatorStatus{
+				Conditions: []configv1.ClusterOperatorStatusCondition{
+					{
+						Type: configv1.OperatorProgressing,
+					},
+					{
+						Type: configv1.OperatorAvailable,
+					},
+					{
+						Type: configv1.OperatorProgressing,
+					},
+				},
+			},
+		},
+	}
+
+	for _, tc := range testCases {
+		if actual := statusesEqual(tc.a, tc.b); actual != tc.expected {
+			t.Fatalf("%q: expected %v, got %v", tc.description, tc.expected, actual)
 		}
 	}
 }
