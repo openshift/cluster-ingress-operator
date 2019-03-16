b5d4fb5c3fae2c72f79dbc598f81c2271cd693cd
Miciah Masters <miciah.masters@gmail.com>
2019-03-16 19:12:56 -0400

    desiredRouterCertsGlobalSecret: Add ns parameter
    * pkg/operator/controller/certificate-publisher/publish_certs.go
    (ensureRouterCertsGlobalSecret): Pass the operand namespace to
    desiredRouterCertsGlobalSecret.
    (desiredRouterCertsGlobalSecret): Add a parameter for the operand
    namespace.  Pass this parameter (instead of the empty string) to
    RouterEffectiveDefaultCertificateSecretName.
    * pkg/operator/controller/certificate-publisher/publish_certs_test.go
    (TestDesiredRouterCertsGlobalSecret): Adjust call to
    desiredRouterCertsGlobalSecret.

--- pkg/operator/controller/certificate-publisher/publish_certs_test.go
+++ pkg/operator/controller/certificate-publisher/publish_certs_test.go
@@ -187,7 +187,7 @@ func TestDesiredRouterCertsGlobalSecret(t *testing.T) {
 
 	for _, tc := range testCases {
 		expected := tc.output.secret
-		actual, err := desiredRouterCertsGlobalSecret(tc.inputs.secrets, tc.inputs.ingresses)
+		actual, err := desiredRouterCertsGlobalSecret(tc.inputs.secrets, tc.inputs.ingresses, "openshift-ingress")
 		if err != nil {
 			t.Errorf("failed to get desired router-ca global secret: %v", err)
 			continue
