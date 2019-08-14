4c56ec1fcfc538eb1801c914d807eec4d5e33775
Miciah Masters <miciah.masters@gmail.com>
2019-08-14 14:48:17 -0400

    ensureServiceMonitor: Update resource if needed
    If a servicemonitor already exists but is not the same as the desired
    servicemonitor, update the current servicemonitor.
    
    Commit 0adb643befab1e435c53aefd7c16e0a84dba5fe5 added a selector to the
    servicemonitor that desiredServiceMonitor returns, so the operator must
    update any servicemonitors created by older versions of the operator.
    
    This commit fixes bug 1740595.
    
    https://bugzilla.redhat.com/show_bug.cgi?id=1740595
    
    * manifests/00-cluster-role.yaml: Allow updating servicemonitors.
    * pkg/operator/controller/ingress/monitoring.go (ensureServiceMonitor): Add
    godoc.  If a servicemonitor is needed and none exists, use
    createServiceMonitor to create it.  If a servicemonitor already exists,
    update it if needed using updateServiceMonitor.  Finally, return a Boolean
    value indicating whether a servicemonitor exists.
    (desiredServiceMonitor): Add godoc.  Fix the type of the "endpoints" field,
    and add a comment why it is important to use the right type.
    (currentServiceMonitor): Add godoc.  Add a Boolean return value indicating
    whether a servicemonitor exists.
    (createServiceMonitor, updateServiceMonitor): New methods, used by
    ensureServiceMonitor.
    (serviceMonitorChanged): New function, used by updateServiceMonitor.
    Compare the specs of the given servicemonitors and return a Boolean value
    indicating whether an update is needed and, if so, an updated
    servicemonitor definition.
    * pkg/operator/controller/ingress/metrics.go (ensureMetricsIntegration):
    Ignore the Boolean return value from ensureServiceMonitor.
    * pkg/operator/controller/ingress/monitoring_test.go
    (TestServiceMonitorChanged): New test.

---
+++ pkg/operator/controller/ingress/monitoring_test.go
@@ -0,0 +1,47 @@
+package ingress
+
+import (
+	"testing"
+
+	operatorv1 "github.com/openshift/api/operator/v1"
+
+	corev1 "k8s.io/api/core/v1"
+
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
+)
+
+func TestServiceMonitorChanged(t *testing.T) {
+	trueVar := true
+	ic := &operatorv1.IngressController{
+		ObjectMeta: metav1.ObjectMeta{
+			Name: "default",
+		},
+	}
+	svc := &corev1.Service{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      "router-default",
+			Namespace: "openshift-ingress",
+		},
+	}
+	deploymentRef := metav1.OwnerReference{
+		APIVersion: "apps/v1",
+		Kind:       "Deployment",
+		Name:       "router-default",
+		UID:        "1",
+		Controller: &trueVar,
+	}
+	sm1 := desiredServiceMonitor(ic, svc, deploymentRef)
+	sm2 := desiredServiceMonitor(ic, svc, deploymentRef)
+	if changed, _ := serviceMonitorChanged(sm1, sm2); changed {
+		t.Fatal("expected changed to be false for two servicemonitors defined for the same ingresscontroller and service")
+	}
+	if err := unstructured.SetNestedField(sm2.Object, nil, "spec", "selector"); err != nil {
+		t.Fatalf("failed to mutate servicemonitor: %v", err)
+	}
+	if changed, sm3 := serviceMonitorChanged(sm1, sm2); !changed {
+		t.Fatal("expected changed to be true after clearing servicemonitor's selector")
+	} else if changedAgain, _ := serviceMonitorChanged(sm2, sm3); changedAgain {
+		t.Fatal("serviceMonitorChanged does not behave as a fixed-point function")
+	}
+}
