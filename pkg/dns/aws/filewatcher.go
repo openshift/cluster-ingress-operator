24d089012418727ee7d3cd7632cfe4bb63e0a711
Daneyon Hansen <daneyonhansen@gmail.com>
2019-12-08 22:21:04 -0800

    Use a reload chan to indicate file watcher updates

--- pkg/dns/aws/filewatcher.go
+++ pkg/dns/aws/filewatcher.go
@@ -2,14 +2,11 @@ package aws
 
 import (
 	"fmt"
-	"time"
 
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/service/elb"
 	"github.com/aws/aws-sdk-go/service/resourcegroupstaggingapi"
 	"github.com/aws/aws-sdk-go/service/route53"
-
-	"k8s.io/apimachinery/pkg/util/wait"
 )
 
 // StartWatcher starts a file watcher and periodically ensures AWS sessions
@@ -17,15 +14,12 @@ import (
 // or error channels.
 func (m *Provider) StartWatcher(operatorReleaseVersion string, stop <-chan struct{}) error {
 	errChan := make(chan error)
-	go wait.Until(func() {
-		err := m.ensureSessionTransport(operatorReleaseVersion)
-		if err != nil {
-			log.Error(err, "failed to ensure dns client transport")
-		}
-	}, 1*time.Minute, stop)
-
+	reloadChan := make(chan bool)
 	go func() {
-		errChan <- m.fileWatcher.Start(stop)
+		errChan <- m.fileWatcher.Start(stop, reloadChan)
+	}()
+	go func() {
+		errChan <- m.ensureSessionTransport(operatorReleaseVersion, reloadChan)
 	}()
 
 	// Wait for the watcher to exit or an explicit stop.
@@ -39,19 +33,20 @@ func (m *Provider) StartWatcher(operatorReleaseVersion string, stop <-chan struc
 
 // ensureSessionTransport ensures AWS sessions use the current certificates
 // from the file watcher.
-func (m *Provider) ensureSessionTransport(operatorReleaseVersion string) error {
-	if m.fileWatcher.FileChanged() {
-		sess, err := NewProviderSession(m.config, operatorReleaseVersion, m.fileWatcher.GetFileData())
-		if err != nil {
-			return fmt.Errorf("failed to create dns provider session: %v", err)
+func (m *Provider) ensureSessionTransport(operatorReleaseVersion string, reloadCh chan bool) error {
+	for {
+		select {
+		case <-reloadCh:
+			sess, err := NewProviderSession(m.config, operatorReleaseVersion, m.fileWatcher.GetFileData())
+			if err != nil {
+				return fmt.Errorf("failed to create dns provider session: %v", err)
+			}
+			m.lock.Lock()
+			m.elb = elb.New(sess, aws.NewConfig().WithRegion(m.config.Region))
+			m.route53 = route53.New(sess)
+			m.tags = resourcegroupstaggingapi.New(sess, aws.NewConfig().WithRegion("us-east-1"))
+			m.lock.Unlock()
+			log.Info("dns provider session updated")
 		}
-		m.lock.Lock()
-		m.elb = elb.New(sess, aws.NewConfig().WithRegion(m.config.Region))
-		m.route53 = route53.New(sess)
-		m.tags = resourcegroupstaggingapi.New(sess, aws.NewConfig().WithRegion("us-east-1"))
-		m.lock.Unlock()
-		m.fileWatcher.SetFileChanged(false)
-		log.Info("updated dns provider session")
 	}
-	return nil
 }
