7263788092b3d8eaee38a983115ee2181668b0c6
Dan Mace <ironcladlou@gmail.com>
2020-03-31 12:21:43 -0400

    Only run dep verification in CI

--- hack/verify-deps.sh
+++ hack/verify-deps.sh
@@ -2,11 +2,16 @@
 set -euo pipefail
 
 function print_failure {
-  echo "There appear to be uncommitted changes to go.mod. Please run 'go tidy' and 'go vendor'."
+  echo "There are unexpected changes to the vendor tree following 'go mod vendor' and 'go mod tidy'. Please"
+  echo "run these commands locally and double-check the Git repository for unexpected changes which may"
+  echo "need to be committed."
   exit 1
 }
 
-go mod vendor
-go mod tidy
+if [ "${OPENSHIFT_CI:-false}" = true ]; then
+  go mod vendor
+  go mod tidy
 
-test -z "$(git status --porcelain)" || print_failure
+  test -z "$(git status --porcelain)" || print_failure
+  echo "verified Go modules"
+fi
