50a2a361c05984fb108926765fa866206635f98d
Miciah Masters <miciah.masters@gmail.com>
2020-05-12 16:57:41 -0400

    dns: Log success or failure to publish record
    * pkg/operator/controller/dns/controller.go (publishRecordToZones): Log
    success or failure to publish the DNS record to a given DNS zone.

--- pkg/operator/controller/dns/controller.go
+++ pkg/operator/controller/dns/controller.go
@@ -153,11 +153,13 @@ func (r *reconciler) publishRecordToZones(zones []configv1.DNSZone, record *iov1
 		}
 
 		if err := r.dnsProvider.Ensure(record, zone); err != nil {
+			log.Error(err, "failed to publish DNS record to zone", "dnsrecord", record, "dnszone", zone)
 			condition.Status = string(operatorv1.ConditionTrue)
 			condition.Reason = "ProviderError"
 			condition.Message = fmt.Sprintf("The DNS provider failed to ensure the record: %v", err)
 			result.RequeueAfter = 30 * time.Second
 		} else {
+			log.Info("published DNS record to zone", "dnsrecord", record, "dnszone", zone)
 			condition.Status = string(operatorv1.ConditionFalse)
 			condition.Reason = "ProviderSuccess"
 			condition.Message = "The DNS provider succeeded in ensuring the record"
