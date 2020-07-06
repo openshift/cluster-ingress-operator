3cc583ffb7b1f9dfb51d6fe24d37cf8a12fd30aa
Andrew McDermott <amcdermo@redhat.com>
2020-07-06 23:21:21 +0100

    deployment: simplify function documentation for HTTP2IsEnabled x2

--- pkg/operator/controller/ingress/deployment.go
+++ pkg/operator/controller/ingress/deployment.go
@@ -98,7 +98,7 @@ func HTTP2IsEnabledByAnnotation(m map[string]string) (bool, bool) {
 
 // HTTP2IsEnabled returns true if the ingress controller enables
 // http/2, or if the ingress config enables http/2. It will return
-// false for the case where the ingress config has been disabled but
+// false for the case where the ingress config has been enabled but
 // the ingress controller explicitly overrides that by having the
 // annotation present (even if its value is "false").
 func HTTP2IsEnabled(ic *operatorv1.IngressController, ingressConfig *configv1.Ingress) bool {
