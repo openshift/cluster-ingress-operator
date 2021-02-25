c31361118f1f609517d31321e57e823ed0639a72
Stephen Greene <sgreene@redhat.com>
2021-02-25 10:56:30 -0500

    Canary: Add node-selector annotation to canary namespace
    assets/canary/namespace.yaml:
    
    Add the `openshift.io/node-selector` annotation to the canary namespace
    yaml file. This will allow the canary daemonset to schedule pods onto
    infra nodes (in addition to worker nodes) by overriding the default
    behavior.
    
    pkg/manifests/bindata.go:
    
    Regenerate.
    
    pkg/operator/controller/canary/namespace.go:
    
    Add logic to update the canary namespace resource on a cluster
    specifically when the namespace's node-selector annotation changes.
    
    pkg/operator/controller/canary/namespace_test.go:
    
    Add a unit test to exercise the new `canaryNamespaceChanged` function.
    
    pkg/operator/controller/canary/controller.go:
    
    Update `ensureCanaryNamespace` calling site.

---
+++ pkg/operator/controller/canary/namespace_test.go
@@ -0,0 +1,45 @@
+package canary
+
+import (
+	"testing"
+
+	"github.com/openshift/cluster-ingress-operator/pkg/manifests"
+
+	corev1 "k8s.io/api/core/v1"
+
+	projectv1 "github.com/openshift/api/project/v1"
+)
+
+func TestCanaryNamespaceChanged(t *testing.T) {
+	testCases := []struct {
+		description string
+		mutate      func(*corev1.Namespace)
+		expect      bool
+	}{
+		{
+			description: "if nothing changes",
+			mutate:      func(_ *corev1.Namespace) {},
+			expect:      false,
+		},
+		{
+			description: "if namespace node-selector annotation changes",
+			mutate: func(ns *corev1.Namespace) {
+				ns.Annotations[projectv1.ProjectNodeSelector] = "foo"
+			},
+			expect: true,
+		},
+	}
+
+	for _, tc := range testCases {
+		original := manifests.CanaryNamespace()
+		mutated := original.DeepCopy()
+		tc.mutate(mutated)
+		if changed, updated := canaryNamespaceChanged(original, mutated); changed != tc.expect {
+			t.Errorf("%s, expect canaryNamespaceChanged to be %t, got %t", tc.description, tc.expect, changed)
+		} else if changed {
+			if changedAgain, _ := canaryNamespaceChanged(mutated, updated); changedAgain {
+				t.Errorf("%s, canaryNamespaceChanged does not behave as a fixed point function", tc.description)
+			}
+		}
+	}
+}
