9f329233f31db93d7e7fa6061e1f514dc02af5dc
Luigi Mario Zuccarelli <luzuccar@redhat.com>
2021-08-06 16:26:41 +0100

    Add e2e test for IngressStatus on DNS config change (degrade status)

--- pkg/operator/controller/ingress/status.go
+++ pkg/operator/controller/ingress/status.go
@@ -713,7 +713,7 @@ func computeDNSStatus(ic *operatorv1.IngressController, wildcardRecord *iov1.DNS
 			for _, cond := range zone.Conditions {
 				if cond.Type == iov1.DNSRecordFailedConditionType && cond.Status == string(operatorv1.ConditionTrue) {
 					// check to see if the zone is in the dnsConfig.Spec
-					// fix:BZ1942657 - relates to status changes when updating DNS privateZone config
+					// fix:BZ1942657 - relates to status changes when updating DNS PrivateZone config
 					if checkZoneInConfig(dnsConfig, zone.DNSZone) {
 						failedZones = append(failedZones, zone.DNSZone)
 					}
@@ -744,20 +744,7 @@ func computeDNSStatus(ic *operatorv1.IngressController, wildcardRecord *iov1.DNS
 // checkZoneInConfig - private utility to check for a zone in the current config
 func checkZoneInConfig(dnsConfig *configv1.DNS, zone configv1.DNSZone) bool {
 
-	// check for public zone ID
-	if dnsConfig.Spec.PublicZone != nil && dnsConfig.Spec.PublicZone.ID != "" && zone.ID != "" {
-		if dnsConfig.Spec.PublicZone.ID == zone.ID {
-			return true
-		}
-	}
-
-	// check for public zone Tags
-	if dnsConfig.Spec.PublicZone != nil && dnsConfig.Spec.PublicZone.Tags["Name"] != "" && zone.Tags["Name"] != "" {
-		if dnsConfig.Spec.PublicZone.Tags["Name"] == zone.Tags["Name"] {
-			return true
-		}
-	}
-
+	// check PrivateZone settings only
 	// check for private zone ID
 	if dnsConfig.Spec.PrivateZone != nil && dnsConfig.Spec.PrivateZone.ID != "" && zone.ID != "" {
 		if dnsConfig.Spec.PrivateZone.ID == zone.ID {
@@ -765,7 +752,7 @@ func checkZoneInConfig(dnsConfig *configv1.DNS, zone configv1.DNSZone) bool {
 		}
 	}
 
-	// check for pirivate zone Tags
+	// check for private zone Tags
 	if dnsConfig.Spec.PrivateZone != nil && dnsConfig.Spec.PrivateZone.Tags["Name"] != "" && zone.Tags["Name"] != "" {
 		if dnsConfig.Spec.PrivateZone.Tags["Name"] == zone.Tags["Name"] {
 			return true
---
+++ test/e2e/dns_ingressdegrade_test.go
@@ -0,0 +1,102 @@
+// +build e2e
+
+package e2e
+
+import (
+	"context"
+	"fmt"
+	"os"
+	"testing"
+
+	configv1 "github.com/openshift/api/config/v1"
+	operatorclient "github.com/openshift/cluster-ingress-operator/pkg/operator/client"
+
+	"sigs.k8s.io/controller-runtime/pkg/client/config"
+
+	"k8s.io/apimachinery/pkg/types"
+)
+
+// TestIngressStatus - degrade/restore status via DNS config
+// This test will check the ingress status
+//
+// Steps :-
+// 1. initial check - should be false
+// 2. update the DNS private zone tags to unknown value
+// 3. check the status should have degraded to true
+// 4. re-instate the original condition
+// 5. check the status should have degraded to false
+func TestIngressStatus(t *testing.T) {
+	kubeConfig, err := config.GetConfig()
+	if err != nil {
+		fmt.Printf("failed to get kube config: %s\n", err)
+		os.Exit(1)
+	}
+	kubeClient, err := operatorclient.NewClient(kubeConfig)
+	if err != nil {
+		t.Errorf("failed to create kube client: %v", err)
+	}
+	kclient = kubeClient
+
+	if err := kclient.Get(context.TODO(), types.NamespacedName{Name: "cluster"}, &dnsConfig); err != nil {
+		t.Errorf("failed to get DNS config: %v", err)
+	}
+
+	// step 1
+	expected := []configv1.ClusterOperatorStatusCondition{
+		{Type: configv1.OperatorAvailable, Status: configv1.ConditionTrue},
+		{Type: configv1.OperatorDegraded, Status: configv1.ConditionFalse},
+	}
+	if err := waitForClusterOperatorConditions(t, kclient, expected...); err != nil {
+		t.Errorf("did not get expected available condition: %v", err)
+	}
+
+	// step 2
+	updCfg := dnsConfig.DeepCopy()
+	updateDNSConfig(true, updCfg)
+	if err := kclient.Update(context.TODO(), updCfg); err != nil {
+		t.Errorf("failed to get DNS config: %v", err)
+	}
+
+	// step 3
+	expected = []configv1.ClusterOperatorStatusCondition{
+		{Type: configv1.OperatorDegraded, Status: configv1.ConditionTrue},
+	}
+	if err := waitForClusterOperatorConditions(t, kclient, expected...); err != nil {
+		t.Errorf("did not get expected available condition: %v", err)
+	}
+
+	// step 4
+	updateDNSConfig(false, updCfg)
+	if err := kclient.Update(context.TODO(), updCfg); err != nil {
+		t.Errorf("failed to get DNS config: %v", err)
+	}
+
+	// step 5
+	expected = []configv1.ClusterOperatorStatusCondition{
+		{Type: configv1.OperatorAvailable, Status: configv1.ConditionTrue},
+		{Type: configv1.OperatorDegraded, Status: configv1.ConditionFalse},
+	}
+	if err := waitForClusterOperatorConditions(t, kclient, expected...); err != nil {
+		t.Errorf("did not get expected available condition: %v", err)
+	}
+}
+
+// updateDNSConfig - utility to set/unset PrivateZone Tags/ID
+func updateDNSConfig(set bool, dnsConfig *configv1.DNS) {
+	if dnsConfig.Spec.PrivateZone.ID != "" {
+		if set {
+			dnsConfig.Spec.PrivateZone.ID = "error-" + dnsConfig.Spec.PrivateZone.ID
+		} else {
+			// remove 'error-' from prefix
+			dnsConfig.Spec.PrivateZone.ID = dnsConfig.Spec.PrivateZone.ID[6:]
+		}
+	}
+	if dnsConfig.Spec.PrivateZone.Tags["Name"] != "" {
+		if set {
+			dnsConfig.Spec.PrivateZone.Tags["Name"] = "error-" + dnsConfig.Spec.PrivateZone.Tags["Name"]
+		} else {
+			// remove 'error-' from prefix
+			dnsConfig.Spec.PrivateZone.Tags["Name"] = dnsConfig.Spec.PrivateZone.Tags["Name"][6:]
+		}
+	}
+}
