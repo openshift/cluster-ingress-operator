f86f32f9e3dbd68bc3be88fbb60e4c04f9c93166
Andrew McDermott <amcdermo@redhat.com>
2024-05-23 14:22:14 +0100

    HACK: Set ImagePullPolicy to Always for ingress controller
    Ensure the ingress controller image is always pulled from the registry
    by setting ImagePullPolicy to Always. This change is crucial for
    debugging, especially when using the -i option with the ingress-operator
    start command.
    
    Modify desiredRouterDeployment function to set the ImagePullPolicy to
    "Always", ensuring the latest image is always used for the ingress
    controller container. Additionally, update the deploymentConfigChanged
    function to synchronise the ImagePullPolicy between the expected and
    current deployment configurations.
    
    These changes ensure that the latest image is pulled whenever a pod is
    created, facilitating a more reliable debugging process by preventing
    the use of outdated local images.

--- pkg/operator/controller/ingress/deployment.go
+++ pkg/operator/controller/ingress/deployment.go
@@ -693,6 +693,8 @@ func desiredRouterDeployment(ci *operatorv1.IngressController, ingressController
 	}
 
 	deployment.Spec.Template.Spec.Containers[0].Image = ingressControllerImage
+	deployment.Spec.Template.Spec.Containers[0].ImagePullPolicy = corev1.PullAlways
+
 	deployment.Spec.Template.Spec.DNSPolicy = corev1.DNSClusterFirst
 
 	var (
@@ -1624,6 +1626,7 @@ func deploymentConfigChanged(current, expected *appsv1.Deployment) (bool, *appsv
 	updated.Spec.Template.Spec.Containers[0].SecurityContext = expected.Spec.Template.Spec.Containers[0].SecurityContext
 	updated.Spec.Template.Spec.Containers[0].Env = expected.Spec.Template.Spec.Containers[0].Env
 	updated.Spec.Template.Spec.Containers[0].Image = expected.Spec.Template.Spec.Containers[0].Image
+	updated.Spec.Template.Spec.Containers[0].ImagePullPolicy = expected.Spec.Template.Spec.Containers[0].ImagePullPolicy
 	copyProbe(expected.Spec.Template.Spec.Containers[0].LivenessProbe, updated.Spec.Template.Spec.Containers[0].LivenessProbe)
 	copyProbe(expected.Spec.Template.Spec.Containers[0].ReadinessProbe, updated.Spec.Template.Spec.Containers[0].ReadinessProbe)
 	copyProbe(expected.Spec.Template.Spec.Containers[0].StartupProbe, updated.Spec.Template.Spec.Containers[0].StartupProbe)
