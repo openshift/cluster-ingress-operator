57964cfaae4e281e544a6ffdada03ddfed3752cf
Ishmam Amin <iamin@redhat.com>
2025-11-17 18:25:48 -0500

    Bump to OSSM 3.2.0 and Istio 1.27.3
    Update the conformance tests timeout to account for GRPCRoute timeout failures which are related to the MaxTimeToConsistency: timeout variable and increase the general timeout to 60m

--- cmd/ingress-operator/start.go
+++ cmd/ingress-operator/start.go
@@ -35,8 +35,8 @@ const (
 	defaultTrustedCABundle           = "/etc/pki/ca-trust/extracted/pem/tls-ca-bundle.pem"
 	defaultGatewayAPIOperatorCatalog = "redhat-operators"
 	defaultGatewayAPIOperatorChannel = "stable"
-	defaultGatewayAPIOperatorVersion = "servicemeshoperator3.v3.1.0"
-	defaultIstioVersion              = "v1.26.2"
+	defaultGatewayAPIOperatorVersion = "servicemeshoperator3.v3.2.0"
+	defaultIstioVersion              = "v1.27.3"
 )
 
 type StartOptions struct {
