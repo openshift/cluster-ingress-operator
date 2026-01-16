3e8173b081aad8e53d25dcb54743f1b2301931b5
Ishmam Amin <iamin@redhat.com>
2026-01-16 14:08:09 -0500

    update gwc name

--- hack/gatewayapi-conformance.sh
+++ hack/gatewayapi-conformance.sh
@@ -62,5 +62,5 @@ SUPPORTED_FEATURES="ReferenceGrant,HTTPRoute,Gateway,GatewayInfrastructurePropag
 SKIPPED_TESTS="HTTPRouteCORSAllowCredentialsBehavior,BackendTLSPolicyConflictResolution,GatewayStaticAddresses"
 
 echo "Start Gateway API Conformance Testing"
-go test ./conformance -v -timeout 60m -run TestConformance -args "--gateway-class=istio" "--report-output=openshift.yaml" "--organization=Red Hat" "--project=Openshift Service Mesh" "--version=3.3.0" "--url=https://www.redhat.com/en/technologies/cloud-computing/openshift/container-platform" "--conformance-profiles=GATEWAY-HTTP,GATEWAY-GRPC" "--supported-features=${SUPPORTED_FEATURES}" "--skip-tests=${SKIPPED_TESTS}"
+go test ./conformance -v -timeout 60m -run TestConformance -args "--gateway-class=conformance" "--report-output=openshift.yaml" "--organization=Red Hat" "--project=Openshift Service Mesh" "--version=3.3.0" "--url=https://www.redhat.com/en/technologies/cloud-computing/openshift/container-platform" "--conformance-profiles=GATEWAY-HTTP,GATEWAY-GRPC" "--supported-features=${SUPPORTED_FEATURES}" "--skip-tests=${SKIPPED_TESTS}"
 cat conformance/openshift.yaml
