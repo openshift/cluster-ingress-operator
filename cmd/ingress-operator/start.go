b04055eb3139b2d7db82dc20780cbef945c10fb7
Ishmam Amin <iamin@redhat.com>
2026-03-19 17:08:03 -0400

    Bump to OSSM 3.3.0 and Istio v1.28.4

--- cmd/ingress-operator/start.go
+++ cmd/ingress-operator/start.go
@@ -35,8 +35,8 @@ const (
 	defaultTrustedCABundle           = "/etc/pki/ca-trust/extracted/pem/tls-ca-bundle.pem"
 	defaultGatewayAPIOperatorCatalog = "redhat-operators"
 	defaultGatewayAPIOperatorChannel = "stable"
-	defaultGatewayAPIOperatorVersion = "servicemeshoperator3.v3.2.2"
-	defaultIstioVersion              = "v1.27.5"
+	defaultGatewayAPIOperatorVersion = "servicemeshoperator3.v3.3.0"
+	defaultIstioVersion              = "v1.28.4"
 )
 
 type StartOptions struct {
