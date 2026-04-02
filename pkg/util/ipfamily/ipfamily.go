c5a60905807343d153af710c1aaa081deb35bbbc
barbacbd <barbacbd@gmail.com>
2026-04-02 07:39:28 -0400

    CORS-4372: Create IPv6 records for GCP
    pkg/util/aws/dualstack.go:
    
    - Removed the "aws specific" capability of determining stack type
    
    pkg/util/ipfamily/ipfamily.go:
    
    - Created the common function to determine stack type dynamically.
    
    pkg/util/ipfamily/ipfamily_test.go:
    
    - Generated tests for the stack determination
    
    pkg/dns/gcp/provider.go:
    
    - Added dual-stack DNS support for GCP *.apps records
    
    pkg/operator/controller/dns/controller.go:
    
    - Add IP family to the controller call.

---
+++ pkg/util/ipfamily/ipfamily.go
@@ -0,0 +1,9 @@
+package ipfamily
+
+import configv1 "github.com/openshift/api/config/v1"
+
+// IsDualStack returns true if the given IPFamilyType indicates a dual-stack
+// configuration.
+func IsDualStack(ipFamily configv1.IPFamilyType) bool {
+	return ipFamily == configv1.DualStackIPv4Primary || ipFamily == configv1.DualStackIPv6Primary
+}
