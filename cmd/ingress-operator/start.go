dcaf2ffcf9496741202b2cd04a588dce9722a7d2
Ishmam Amin <iamin@redhat.com>
2026-06-01 18:19:53 -0400

    revert istio version back to v1.28.5

--- cmd/ingress-operator/start.go
+++ cmd/ingress-operator/start.go
@@ -39,7 +39,7 @@ const (
 	// OSSM 3.3.1 ships Istio 1.28.5.
 	// Using floating z-stream tag to avoid breaking clusters with
 	// customer-managed subscriptions on a different OSSM minor version.
-	defaultIstioVersion = "v1.28-latest"
+	defaultIstioVersion = "v1.28.5"
 )
 
 type StartOptions struct {
